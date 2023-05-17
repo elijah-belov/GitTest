e34f3a9db319f9862dd0ff7755ffbc06166c3677
Elijah Belov <elijahbelov2020@gmail.com>
2023-05-17 12:25:54 -0400

    Initial Commit

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by elijah dev on 5/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        print("git")
+        
+    }
+    
+}
+
